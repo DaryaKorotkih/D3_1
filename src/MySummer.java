2e8edcb7ea576edf93edc4edcc0c4796f4005cf3
Darya Korotkih <riyafonte@yahoo.com>
2017-11-11 23:20:26 +0300

    добавлен текст

--- src/MySummer.java
+++ src/MySummer.java
@@ -1,5 +1,9 @@
 public class MySummer {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("\n Лета я ждал весь год.\n " +
+                "Сначала я подожгу школу.\n" +
+                " Потом убью математичку,\n " +
+                "потом географичку и трудовика.\n " +
+                "А потом поеду на речку Вилюй мыть золото…");
     }
 }
